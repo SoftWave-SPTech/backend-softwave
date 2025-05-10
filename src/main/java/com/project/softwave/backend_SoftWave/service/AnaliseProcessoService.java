d993da964fd5d365a6feee50befbea2abce87806
letfonsecaa <leticia.santos@sptech.school>
2025-05-09 23:07:27 -0300

    fix: endpoints IA

--- src/main/java/com/project/softwave/backend_SoftWave/service/AnaliseProcessoService.java
+++ src/main/java/com/project/softwave/backend_SoftWave/service/AnaliseProcessoService.java
@@ -1,6 +1,7 @@
 package com.project.softwave.backend_SoftWave.service;
 
 import com.project.softwave.backend_SoftWave.entity.AnaliseProcesso;
+import com.project.softwave.backend_SoftWave.exception.EntidadeNaoEncontradaException;
 import com.project.softwave.backend_SoftWave.repository.AnaliseProcessoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,24 @@ public class AnaliseProcessoService {
     @Autowired
     private AnaliseProcessoRepository analiseRepository;
 
+
     public List<AnaliseProcesso> listarTodas() {
         return analiseRepository.findAll();
+    }
+
+
+    public AnaliseProcesso buscarPorId(Integer id) {
+
+        return analiseRepository.findById(id).orElseThrow(
+                () -> new EntidadeNaoEncontradaException("Análise IA com ID %d não foi encontrada".formatted(id))
+        );
+    }
+
+    public AnaliseProcesso buscarPorIdMovimentacao(Integer movimentacaoId) {
+
+        return analiseRepository.findByMovimentacoesId(movimentacaoId).orElseThrow(
+                () -> new EntidadeNaoEncontradaException(
+                        "Nenhuma análise encontrada para a movimentação com ID %d".formatted(movimentacaoId))
+        );
     }
 }
