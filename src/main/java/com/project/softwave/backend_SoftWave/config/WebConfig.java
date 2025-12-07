caa20031445402d9582a19cd3c88697cec96a601
Lua ♡ <160372370+lualuaSantosS2@users.noreply.github.com>
2025-12-07 12:55:36 -0300

    Update WebConfig.java

--- src/main/java/com/project/softwave/backend_SoftWave/config/WebConfig.java
+++ src/main/java/com/project/softwave/backend_SoftWave/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.project.softwave.backend_SoftWave.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;               // <--- adicionado
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -43,4 +44,3 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("Set-Cookie"); // Permite exposição de cookies
     }
 }
-
